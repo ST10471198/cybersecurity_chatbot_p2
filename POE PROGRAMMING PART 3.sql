1e7b892e9ad5880e5b56bee1dc1b342c051d30f8
ST10471198 <ST10471198@rcconnect.edu.za>
2026-06-25 23:45:01 +0200

    Added comments

---
+++ POE PROGRAMMING PART 3.sql
@@ -0,0 +1,20 @@
+﻿-- Create the database
+CREATE DATABASE chatbot_tasks;
+
+-- Use the database
+USE chatbot_tasks;
+
+-- Create the tasks table
+CREATE TABLE tasks (
+    task_id INT PRIMARY KEY IDENTITY(1,1),
+    username VARCHAR(50) NOT NULL,
+    task_name VARCHAR(100) NOT NULL,
+    task_description VARCHAR(200) NOT NULL,
+    task_due_date VARCHAR(20),
+    task_status VARCHAR(20) DEFAULT 'Pending',
+    reminder_date VARCHAR(20),
+    date_created DATETIME DEFAULT GETDATE()
+);
+
+-- Verify the table was created
+SELECT * FROM tasks;
